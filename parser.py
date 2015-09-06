39147ed4cd046b0e226bf5641f1624b4c117617e
Aris Poloway <agpoloway@gmail.com>
2015-09-06 16:50:56 -0600

    Initial commit

---
+++ parser.py
@@ -0,0 +1,36 @@
+import random
+
+class SentenceParser(object):
+    def __init__(self):
+        self.words = {}
+
+    def parse(self, words):
+        word_list = words.split(" ")
+        for i, word in enumerate(word_list):
+            if word not in self.words:
+                self.words[word] = {}
+            if i < (len(word_list) - 1):
+                if word_list[i+1] not in self.words[word]:
+                    self.words[word][word_list[i+1]] = 0
+                self.words[word][word_list[i+1]] += 1
+
+    def make_sentence(self, start_word):
+        if start_word not in self.words:
+            return ("Initial word not in list of words")
+        sentence = [start_word]
+        cur_word = start_word
+
+        while(not "." in cur_word):
+            choices = []
+            for choice in self.words[cur_word]:
+                for i in range(self.words[cur_word][choice]):
+                        choices.append(choice)
+            cur_word = random.choice(choices)
+            sentence.append(cur_word)
+
+        return " ".join(sentence)
+
+    def parse_file(self, filename):
+        with open(filename) as f:
+            s = f.read().replace("\n", "")
+            self.parse(s)
